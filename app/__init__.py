926843ad067e5b896c5722349c8d63245af6e99f
Nyash-Mauro <mauricee423@gmail.com>
2020-10-18 16:35:46 +0300

    added register blueprint and app configurations to init.py of app root folder

---
+++ app/__init__.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from config import config_options
+
+bootsrap = Bootstrap()
+
+
+def create_app(config_name):
+
+    app = Flask(__name__)
+
+    # create app configs
+    app.config.from_object
+    (config_options[config_name])
+    config_options[config_name].init_app(app)
+
+    # reg a blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
