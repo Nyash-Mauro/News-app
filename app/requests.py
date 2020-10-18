f94f57ade811633c58dbaa4f6847bc20e97aaaed
Nyash-Mauro <mauricee423@gmail.com>
2020-10-18 17:38:33 +0300

    function thatreturns list of nes objects into a list

--- app/requests.py
+++ app/requests.py
@@ -40,3 +40,26 @@ def get_sources(category):
             sources_results_list = get_sources_response['sources']
             sources_results = process_sources(sources_results_list)
     return sources_results
+
+
+def process_sources(sources_list):
+    """
+    function that processs the news sourecs results and turn them into list of objects
+    Args:
+        source_list: list of dictionaries that contain sources details
+    Returns:
+        sources results,a list of sources objects
+    """
+    sources_results = []
+    for source_item in sources_list:
+        id = source_item.get('id')
+        name = source_item.get('name')
+        description = source_item.get('description')
+        url = source_item.get('url')
+        category = source_item.get('category')
+        language = source_item.get('language')
+        country = source_item.get('country')
+        sources_object = Sources(
+            id, name, description, url, category, language, country)
+        sources_results.append(sources_object)
+    return sources_results
