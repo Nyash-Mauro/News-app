d6c13d4cee79e11e7bd2b79db263d0663661af20
Nyash-Mauro <mauricee423@gmail.com>
2020-10-18 19:16:58 +0300

    added configurations

--- config.py
+++ config.py
@@ -9,4 +9,15 @@ class Config:
     @staticmethod
     def init_app(app):
         pass
-        
+
+class ProdConfig(Config):
+    pass
+
+
+class DevConfig(Config):
+    Debug = True
+
+config_options = {
+    'development':DevConfig,
+    'production':ProdConfig
+}
