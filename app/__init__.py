51b34a6d6ded3eefcca4f51283764f07ad6bab27
Nyash-Mauro <mauricee423@gmail.com>
2020-10-18 16:44:01 +0300

    added classes to define sources objects

--- app/__init__.py
+++ app/__init__.py
@@ -17,3 +17,5 @@ def create_app(config_name):
     # reg a blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+
+    return app
---
+++ app/models.py
@@ -0,0 +1,13 @@
+class Sources:
+    """
+    sources clss to define sources objects
+    """
+
+    def __init__(self, id, name, description, url, category, country, language):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.url = url
+        self.category = category
+        self.country = country
+        self.language = language
