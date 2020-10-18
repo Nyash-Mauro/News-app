d3ae547d0cc6a08ad08cb0c02442ff743b4c0568
Nyash-Mauro <mauricee423@gmail.com>
2020-10-18 16:26:07 +0300

    added function to return populer sources of news

--- app/main/views.py
+++ app/main/views.py
@@ -1,14 +1,15 @@
-from flask import render_template,request,redirect,url_for
+from flask import render_template, request, redirect, url_for
 
 from . import main
-from ..requests import get_sources,get_articles
+from ..requests import get_sources, get_articles
 
 from ..models import Sources
 
 # Views
+
+
 @main.route('/')
 def index():
-
     '''
     View root page function that returns the index page and its data
     '''
@@ -20,22 +21,15 @@ def index():
 
     title = 'News Highlights'
 
-    
-    return render_template('index.html', title = title, sports_sources = sports_sources, health_sources= health_sources,technology_sources = technology_sources )
+    return render_template('index.html', title=title, sports_sources=sports_sources, health_sources=health_sources, technology_sources=technology_sources)
 
 
 @main.route('/articles/<id>')
 def articles(id):
-
     '''
     View sources page function that returns the sources details page and its data
     '''
     articles = get_articles(id)
     title = f'NH{id}'
-    
-    return render_template('articles.html',title = title,articles = articles)
 
-
-
-
-    
+    return render_template('articles.html', title=title, articles=articles)
